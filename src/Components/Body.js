f019300ec2a31cea52d2b76c8a482043d728a51f
Abhik Ray <abhikaboy@gmail.com>
2024-09-28 08:06:21 -0400

    meow finally

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -15,113 +15,186 @@ export default function Body() {
 	const [evalData, setEvalData] = useState([]);
 	const [evalsCurrent, setEvalsCurrent] = useState([]);
 
+	const [invScore, setInvScore] = useState([]);
+	const [prepScores, setPrepScores] = useState([]);
+
+	const [invScoreRaw, setInvRaw] = useState([]);
+	const [prepScoreRaw, setPrepRaw] = useState([]);
+
+	const [prepBreakdownRaw, setPrepBreakdownRaw] = useState([]);
+	const [prepBreakdown, setPrepBreakdown] = useState([]);
+
+	const [presentaionBreakdownRaw, setPresentaionBreakdownRaw] = useState([]);
+	const [presentaionBreakdown, setPresentaionBreakdown] = useState([]);
+
+	const [interviewData, setInterviewData] = useState([]);
+
+	const [interviewDataSelected, setInterviewDataSelected] = useState({
+		name: '',
+		questions: [],
+		responses: [],
+	});
+
 	const handleClick = (e) => {
 		let index = e.target.value;
 		setSelected(index);
 
 		console.log(appData[index]);
 
-		let evalList = evalData.filter(
-			(evaluation) => evaluation.name.toLowerCase().trim() == appData[index].name.toLowerCase().trim(),
+		const name = appData[index].name.toLowerCase().trim();
+		console.log(name);
+
+		console.log(prepScoreRaw[0].name);
+
+		let evalList = evalData.filter((evaluation) => evaluation.name.toLowerCase().trim() == name);
+		let invScoreList = invScoreRaw.filter((evaluation) => evaluation.name.toLowerCase().trim() == name);
+		let prepScoreList = prepScoreRaw.filter((evaluation) => evaluation.name.toLowerCase().trim() == name);
+		let prepBreakdownList = prepBreakdownRaw.filter((evaluation) => evaluation.name.toLowerCase().trim() == name);
+		let presBreakdownList = presentaionBreakdownRaw.filter(
+			(evaluation) => evaluation.name.toLowerCase().trim() == name.split(' ')[0],
 		);
+
+		setInterviewDataSelected(interviewData.filter((interview) => interview.name.toLowerCase().trim() == name)[0]);
+		console.log(interviewData.filter((interview) => interview.name.toLowerCase().trim() == name));
 		setEvalsCurrent(evalList);
+		setInvScore(invScoreList);
+		setPrepScores(prepScoreList);
+		setPrepBreakdown(prepBreakdownList);
+		setPresentaionBreakdown(presBreakdownList);
 	};
 
 	/* 
 		PARSING
 	*/
 
-	let blacklist = [
-		'Aaron Thomas',
-		'Aditya Patwal',
-		'Aishani Raju',
-		'Ashley Shin',
-		'Ava Baichi',
-		'Ben Wanderman',
-		'Clare Murray',
-		'Connor Tukey',
-		'Dia Brar',
-		'Eva Rankin',
-		'Gianna Eberhardt',
-		'Grace Cocorocchia',
-		'Isabella Quintero',
-		'Jack Larkin',
-		'Jonathan Frenz',
-		'Joseph Asoofi',
-		'Juan Cadierno Redondo',
-		'Kathryn Fisher',
-		'Kyle Lee',
-		'Lexi Kemmer',
-		'Merton (Rocky) Rockney',
-		'Mia Khan',
-		'Nicole Swan',
-		'Nikk Shah',
-		'Richard Huang',
-		'Riya Ramdev',
-		'Ross Kobayashi',
-		'Saimaa Malhotra',
-		'Vaunshal Saraiya',
-		'Aarzu Choudhary',
-		'Aidan Ormsby',
-		'Arkin Sawhney',
-		'Caden Braun',
-		'Charlotte Napawan',
-		'Thanawan "Charlotte" Napawan',
-		'Izze (Izadora) D’Farley',
-		'Jay Leung',
-		'London Jones',
-		'Mahika Modi',
-		'Maximus Mcelroy',
-		'Natalie Downs',
-		'Alina Jiang',
-		'Alisha Srivastava',
-		'Ayah Bouchouary',
-		'Chris Catalan Valdez',
-		'Christina Fu',
-		'Diana Heung',
-		'Esabella DeFilippo',
-		'Frank Giugliano',
-		'Jack Jiang',
-		'Jack McCarthy',
-		'Jaden Chin',
-		'Joshua Lin',
-		'Leran Ban',
-		'Liam Sutton',
-		'Meredith Lee',
-		'Michael Zemedeneh',
-		'Nicolas Somma Tang',
-		'Orelia Thottam',
-		'Sameera Boga',
-		'Shreya Dandu',
-		'Siran Khachatourian',
-		'Trayna Bui',
-		'Vanini Agrawal',
-		'Vedanta Sharma',
-		'Lily Bailey',
-		'Arjun Vellimedu',
-		'Kristen Liao',
+	let url = 'https://relay-file-upload.s3-us-east-2.amazonaws.com/elxnaqi5pqgAKPsiFL24APPS-FormResponses1(6).csv'; // `https://relay-file-upload.s3-us-east-2.amazonaws.com/elxnaqi5pqgAKPsiFL24APPS-FormResponses1(6).csv`;
+	let evalUrl =
+		'https://relay-file-upload.s3-us-east-2.amazonaws.com/i3a7f63aklpUpsilonEvalForm!(Responses)-FormResponses1(4).csv'; // `https://relay-file-upload.s3-us-east-2.amazonaws.com/i3a7f63aklpUpsilonEvalForm!(Responses)-FormResponses1(4).csv`;
+	let evalAggregate = `,`;
+
+	let finalInvScore = `https://relay-file-upload.s3-us-east-2.amazonaws.com/u1ba9tc9habUpsilonDelibsMastersheet-FinalTotalAverages(invitationalsprepandpres)(1).csv`;
+	let prepScoresCSV = `https://relay-file-upload.s3-us-east-2.amazonaws.com/xsfqcrb69tUpsilonDelibsMastersheet-InvitationalsEvals_Comments.csv`;
+
+	let prepBreakdownCSV = `https://relay-file-upload.s3-us-east-2.amazonaws.com/6kyde4u8egqUpsilonDelibsMastersheet-Invitationalspreparationaverages.csv`;
+	let presentaionBreakdownCSV = `https://relay-file-upload.s3-us-east-2.amazonaws.com/u5qex8m8wz9UpsilonDelibsMastersheet-Presentationsaverages(1).csv`;
+
+	let interviewUrl = `https://relay-file-upload.s3-us-east-2.amazonaws.com/16kivrxw9r2InterviewTemplate-Sheet1(1).csv`;
+
+	let blacklist = ['', ' '];
+	let whitelist = [
+		`Sean Davis`,
+		`Jack Pereira`,
+		`Aum Pourohit`,
+		`Nichole Chen`,
+		`Megha Iyer`,
+		`Brianna Giangrasso`,
+		`Rebekah Campus-Hartman`,
+		`Kayla Handley`,
+		`Patrick McMorrow`,
+		`Will Marris`,
+		`Galia Koonyevsky`,
+		`Isabella Lam`,
+		`Rishi Kamtam`,
+		`Pallavi Shankar`,
+		`Bonisha Maitra`,
+		`Ethan Kim`,
+		`Liam Clarke`,
+		`Tennyson Hopley-Romig`,
+		`Yuna Shin`,
+		`Rhianna D'Silva`,
+		`Amanda Gomes`,
+		`Tarun Singh`,
 	];
-
-	let url = ''; // `https://relay-file-upload.s3-us-east-2.amazonaws.com/elxnaqi5pqgAKPsiFL24APPS-FormResponses1(6).csv`;
-	let evalUrl = ''; // `https://relay-file-upload.s3-us-east-2.amazonaws.com/i3a7f63aklpUpsilonEvalForm!(Responses)-FormResponses1(4).csv`;
-	let evalAggregate = ``;
-
 	useEffect(() => {
 		Papa.parse(url, {
 			download: true,
 			header: true,
 			complete: function (results) {
 				setHeaders(results.data[1]);
-				setAppData(results.data.slice(1));
-				console.log(results.data.slice(1));
+				setAppData(results.data.slice(1).filter((data) => whitelist.includes(data.name)));
 			},
 		});
+
+		Papa.parse(finalInvScore, {
+			download: true,
+			header: true,
+			complete: function (results) {
+				setInvScore(results.data.slice(1).slice(0, 22));
+				setInvRaw(results.data.slice(1).slice(0, 22));
+			},
+		});
+
+		Papa.parse(prepScoresCSV, {
+			download: true,
+			header: true,
+			complete: function (results) {
+				setPrepScores(results.data.slice(1));
+				setPrepRaw(results.data.slice(1));
+				console.log('PREP BRABH');
+				console.log(prepScoreRaw);
+			},
+		});
+		Papa.parse(presentaionBreakdownCSV, {
+			download: true,
+			header: true,
+			complete: function (results) {
+				setPresentaionBreakdownRaw(results.data.slice(1));
+				setPresentaionBreakdown(results.data.slice(1)[0]);
+				console.log('PREP BRABH');
+				console.log(prepScoreRaw);
+			},
+		});
+
 		Papa.parse(evalUrl, {
 			download: true,
 			header: true,
 			complete: function (results) {
 				setEvalData(results.data.slice(1));
 				console.log(results.data.slice(1));
+			},
+		});
+
+		Papa.parse(prepBreakdownCSV, {
+			download: true,
+			header: true,
+			complete: function (results) {
+				setPrepBreakdownRaw(results.data.slice(1));
+				console.log(results.data.slice(1));
+			},
+		});
+
+		Papa.parse(interviewUrl, {
+			download: true,
+			header: true,
+			complete: function (results) {
+				let interviewDataLocal = [];
+				for (let [i, value] of results.data.entries()) {
+					if (value.Person == '*') {
+						// Found a marker
+						const q = Object.values(results.data[i]); // questions
+						const res = Object.values(results.data[i + 1]); // answers
+						console.log(results.data[i + 1]); // answers
+						let interviewObject = {
+							name: '',
+							questions: [],
+							responses: [],
+						};
+						interviewObject.name = res[0];
+						let questions = q.splice(2);
+						let responses = res.splice(2);
+
+						for (let i = 0; i < responses.length; i++) {
+							if (responses[i].length > 10) {
+								interviewObject.questions.push(questions[i]);
+								interviewObject.responses.push(responses[i]);
+							}
+						}
+						console.log(interviewObject);
+						interviewDataLocal.push(interviewObject);
+						setInterviewDataSelected(interviewObject);
+					}
+				}
+				setInterviewData(interviewDataLocal);
 			},
 		});
 	}, []);
@@ -187,7 +260,15 @@ export default function Body() {
 					})}
 					{/* <option value={selected}>{appData[selected].FullName}</option> */}
 				</select>
-				<Info data={appData[selected]} evalRaw={evalsCurrent} />
+				<Info
+					data={appData[selected]}
+					evalRaw={evalsCurrent}
+					prepBreakdown={prepBreakdown}
+					invScore={invScore}
+					prepScores={prepScores}
+					interview={interviewDataSelected}
+					presBreakdown={presentaionBreakdown}
+				/>
 			</div>
 		</div>
 	);
--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import QA from './QA';
-import app from '../App/application.csv';
-import Papa from 'papaparse';
-import { transform } from 'lodash';
-export default function Info({ data, evalRaw }) {
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+export default function Info({ data, evalRaw, invScore, prepScores, interview, prepBreakdown, presBreakdown }) {
 	// professional	willingness	brotherhood	teamwork	rush_benefit	akpsi_benefit  overall
 	const [averages, setAverages] = useState([]);
+	console.log(interview);
+	console.log(prepBreakdown);
 	useEffect(() => {
 		if (evalRaw.length === 0) {
 			setAverages([0, 0, 0, 0, 0, 0, 0]);
@@ -51,102 +53,217 @@ export default function Info({ data, evalRaw }) {
 			</div>
 		);
 	}
-
-	console.log('Raw Eval');
-	console.log(evalRaw);
+	const computeStyle = (score) => {
+		if (score >= 4) {
+			return { backgroundColor: `#00ff0090` };
+		}
+		if (score >= 3.6) {
+			return { backgroundColor: `#b0ff0080` };
+		}
+		if (score >= 3) {
+			return { backgroundColor: `#60700040` };
+		} else {
+			return { backgroundColor: `#ff000090` };
+		}
+	};
+	console.log('Raw Prep');
+	console.log(prepScores);
+	console.log(invScore);
 	return (
 		<div style={{ backgroundColor: '#fff', bottom: 0, marginBottom: '-5%', paddingBottom: '5%', marginTop: '-5%' }}>
 			<div style={{ marginLeft: '5%', marginTop: '5%', paddingTop: '5%', padding: '2%' }}>
-				<h2>Evaluation Scores</h2>
-				<table
-					style={{
-						width: '95%',
-						boxShadow: '0px 2px 6px 0px rgba(0,0,0,0.2)',
-						borderRadius: 10,
-						color: 'white',
-						padding: 0,
-						spacing: 0,
-						gap: 0,
-					}}>
-					<thead
-						style={{
-							backgroundColor: '#001B2A',
-							padding: 0,
-							height: 50,
-							borderRadius: 10,
-							transform: 'translateX(7px)',
-						}}>
-						<th style={evalheading}>Overall</th>
-						<th style={evalheading}>Professionalism</th>
-						<th style={evalheading}>Willingness</th>
-						<th style={evalheading}>Brotherhood</th>
-						<th style={evalheading}>Teamwork</th>
-						<th style={evalheading}>Rush Benefit</th>
-						<th style={evalheading}>AKPsi Benefit</th>
-					</thead>
-
-					<tr style={{ textAlign: 'center', backgroundColor: '#fff', color: '#001B2A', height: 40 }}>
-						<td>{averages[6]}</td>
-						<td>{averages[0]}</td>
-						<td>{averages[1]}</td>
-						<td>{averages[2]}</td>
-						<td>{averages[3]}</td>
-						<td>{averages[4]}</td>
-						<td>{averages[5]}</td>
-					</tr>
-				</table>
-
-				<div>
-					<h2>
-						Comments{' '}
-						<div
+				<Tabs>
+					<TabList style={{ marginLeft: '-7%' }}>
+						<Tab>Invitational</Tab>
+						<Tab>Interview</Tab>
+						<Tab>Rush</Tab>
+					</TabList>
+					<TabPanel>
+						<h2>Evaluation Scores</h2>
+						<table
 							style={{
-								fontSize: '1rem',
-								display: 'inline',
-								fontWeight: 'normal',
-							}}>{`(${evalRaw.length})`}</div>
-					</h2>
-					{evalRaw.map((evaluation, i) => {
-						return (
-							<div
+								width: '95%',
+								boxShadow: '0px 2px 6px 0px rgba(0,0,0,0.2)',
+								borderRadius: 10,
+								color: 'white',
+								padding: 0,
+								spacing: 0,
+								gap: 0,
+							}}>
+							<thead
 								style={{
-									// border: '1px solid black',
+									backgroundColor: '#001B2A',
+									padding: 0,
+									height: 50,
 									borderRadius: 10,
-									padding: '2%',
-									width: '90%',
-									marginTop: '2%',
-									boxShadow: '0px 2px 6px 0px rgba(0,0,0,0.2)',
+									transform: 'translateX(7px)',
 								}}>
-								<b>
-									{evaluation.brother} ({evaluation.event}):
-								</b>{' '}
-								{evaluation.notes}
-							</div>
-						);
-					})}
-				</div>
+								<th style={evalheading}>Final</th>
+								<th style={evalheading}>Preperation</th>
+								<th style={evalheading}>Presentation</th>
+							</thead>
 
-				<h2>Application</h2>
-				<div style={{ marginTop: '2px', marginBottom: '2px' }}>
-					<QA
-						q='Please describe why you would like to become a brother of Alpha Kappa Psi (150 words or less).'
-						a={data.q1}
-					/>
-					<QA q='What about Alpha Kappa Psi gets you excited and why? (150 words or less)' a={data.q2} />
-					<QA
-						q='What do you think you can bring to our chapter? Why do you think you stand out from the other candidates? (150 words or less)'
-						a={data.q3}
-					/>
-					<QA
-						q='Share something you want us to know that would make you a stronger candidate and has yet to be
+							<tr style={{ textAlign: 'center', backgroundColor: '#fff', color: '#001B2A', height: 40 }}>
+								<td style={computeStyle(invScore[0].final)}>{invScore[0].final}</td>
+								<td style={computeStyle(invScore[0].prep)}>{invScore[0].prep}</td>
+								<td style={computeStyle(invScore[0].eval)}>{invScore[0].eval}</td>
+							</tr>
+						</table>
+						<h2>Preparation Phase</h2>
+						<table
+							style={{
+								width: '95%',
+								boxShadow: '0px 2px 6px 0px rgba(0,0,0,0.2)',
+								borderRadius: 10,
+								color: 'white',
+								padding: 0,
+								spacing: 0,
+								gap: 0,
+							}}>
+							<thead
+								style={{
+									backgroundColor: '#001B2A',
+									padding: 0,
+									height: 50,
+									borderRadius: 10,
+									transform: 'translateX(7px)',
+								}}>
+								<th style={evalheading}>Overall</th>
+								<th style={evalheading}>Leadership</th>
+								<th style={evalheading}>Safe Working Envrionment</th>
+								<th style={evalheading}>Team Players</th>
+							</thead>
+
+							<tr style={{ textAlign: 'center', backgroundColor: '#fff', color: '#001B2A', height: 40 }}>
+								<td style={computeStyle(prepBreakdown[0].average)}>{prepBreakdown[0].average}</td>
+								<td style={computeStyle(prepBreakdown[0].leadership)}>{prepBreakdown[0].leadership}</td>
+								<td style={computeStyle(prepBreakdown[0].environment)}>
+									{prepBreakdown[0].environment}
+								</td>
+								<td style={computeStyle(prepBreakdown[0].team)}>{prepBreakdown[0].team}</td>
+							</tr>
+						</table>
+						<h2>Presentation Phase</h2>
+						<table
+							style={{
+								width: '95%',
+								boxShadow: '0px 2px 6px 0px rgba(0,0,0,0.2)',
+								borderRadius: 10,
+								color: 'white',
+								padding: 0,
+								spacing: 0,
+								gap: 0,
+							}}>
+							<thead
+								style={{
+									backgroundColor: '#001B2A',
+									padding: 0,
+									height: 50,
+									borderRadius: 10,
+									transform: 'translateX(7px)',
+								}}>
+								<th style={evalheading}>Average</th>
+								<th style={evalheading}>Presentation Skills</th>
+								<th style={evalheading}>Collaboration</th>
+								<th style={evalheading}>Group Organization</th>
+								<th style={evalheading}>Quality of Q&A</th>
+								<th style={evalheading}>Overall</th>
+							</thead>
+
+							<tr style={{ textAlign: 'center', backgroundColor: '#fff', color: '#001B2A', height: 40 }}>
+								<td style={computeStyle(presBreakdown[0].average)}>{presBreakdown[0].average}</td>
+								<td style={computeStyle(presBreakdown[0].skill)}>{presBreakdown[0].skill}</td>
+								<td style={computeStyle(presBreakdown[0].collab)}>{presBreakdown[0].collab}</td>
+								<td style={computeStyle(presBreakdown[0].organized)}>{presBreakdown[0].organized}</td>
+								<td style={computeStyle(presBreakdown[0].qna)}>{presBreakdown[0].qna}</td>
+								<td style={computeStyle(presBreakdown[0].overall)}>{presBreakdown[0].overall}</td>
+							</tr>
+						</table>
+
+						<div>
+							<h2>Invitational Prep Comments</h2>
+						</div>
+						{prepScores.map((evaluation, i) => {
+							return (
+								<div
+									style={{
+										// border: '1px solid black',
+										borderRadius: 10,
+										padding: '2%',
+										width: '90%',
+										marginTop: '2%',
+										boxShadow: '0px 2px 6px 0px rgba(0,0,0,0.2)',
+									}}>
+									<b>{evaluation.brother}:</b> {evaluation.comment}
+								</div>
+							);
+						})}
+					</TabPanel>
+					<TabPanel>
+						<div>
+							<h2>Interview</h2>
+							{interview.questions.map((question, i) => {
+								return <QA q={question} a={interview.responses[i]} />;
+							})}
+						</div>
+					</TabPanel>
+					<TabPanel>
+						<div>
+							<h2>
+								Rush Comments{' '}
+								<div
+									style={{
+										fontSize: '1rem',
+										display: 'inline',
+										fontWeight: 'normal',
+									}}>{`(${evalRaw.length})`}</div>
+							</h2>
+							{evalRaw.map((evaluation, i) => {
+								return (
+									<div
+										style={{
+											// border: '1px solid black',
+											borderRadius: 10,
+											padding: '2%',
+											width: '90%',
+											marginTop: '2%',
+											boxShadow: '0px 2px 6px 0px rgba(0,0,0,0.2)',
+										}}>
+										<b>
+											{evaluation.brother} ({evaluation.event}):
+										</b>{' '}
+										{evaluation.notes}
+									</div>
+								);
+							})}
+						</div>
+						<h2>Application</h2>
+						<div style={{ marginTop: '2px', marginBottom: '2px' }}>
+							<QA
+								q='Please describe why you would like to become a brother of Alpha Kappa Psi (150 words or less).'
+								a={data.q1}
+							/>
+							<QA
+								q='What about Alpha Kappa Psi gets you excited and why? (150 words or less)'
+								a={data.q2}
+							/>
+							<QA
+								q='What do you think you can bring to our chapter? Why do you think you stand out from the other candidates? (150 words or less)'
+								a={data.q3}
+							/>
+							<QA
+								q='Share something you want us to know that would make you a stronger candidate and has yet to be
 fully represented through your resume or application. (150 words or less)'
-						a={data.q4}
-					/>
-				</div>
-				<h2>Resume</h2>
-				<div style={{ marginTop: '2px', marginBottom: '2px' }}>
-					<iframe src={data.resume} width='94%' height='200px'></iframe>
-				</div>
+								a={data.q4}
+							/>
+						</div>
+						<h2>Resume</h2>
+						<div style={{ marginTop: '2px', marginBottom: '2px' }}>
+							<iframe src={data.resume} width='94%' height='200px'></iframe>
+						</div>
+					</TabPanel>
+				</Tabs>
+
 				{/* <h2>Interview</h2>
 
 				<div style={{ marginTop: '2px', marginBottom: '2px' }}>
