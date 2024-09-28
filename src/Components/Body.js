00bc630a216ff92df7635dcf1b4e7cd42dc19dfb
Abhik Ray <abhikaboy@gmail.com>
2024-09-28 08:48:55 -0400

    meow finally 2

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -67,24 +67,101 @@ export default function Body() {
 		PARSING
 	*/
 
-	let url = 'https://relay-file-upload.s3-us-east-2.amazonaws.com/elxnaqi5pqgAKPsiFL24APPS-FormResponses1(6).csv'; // `https://relay-file-upload.s3-us-east-2.amazonaws.com/elxnaqi5pqgAKPsiFL24APPS-FormResponses1(6).csv`;
+	let url = 'https://relay-file-upload.s3-us-east-2.amazonaws.com/7x4d42n69jsAKPsiFL24APPS-FormResponses1(8).csv'; // `https://relay-file-upload.s3-us-east-2.amazonaws.com/elxnaqi5pqgAKPsiFL24APPS-FormResponses1(6).csv`;
 	let evalUrl =
 		'https://relay-file-upload.s3-us-east-2.amazonaws.com/i3a7f63aklpUpsilonEvalForm!(Responses)-FormResponses1(4).csv'; // `https://relay-file-upload.s3-us-east-2.amazonaws.com/i3a7f63aklpUpsilonEvalForm!(Responses)-FormResponses1(4).csv`;
 	let evalAggregate = `,`;
 
-	let finalInvScore = `https://relay-file-upload.s3-us-east-2.amazonaws.com/u1ba9tc9habUpsilonDelibsMastersheet-FinalTotalAverages(invitationalsprepandpres)(1).csv`;
-	let prepScoresCSV = `https://relay-file-upload.s3-us-east-2.amazonaws.com/xsfqcrb69tUpsilonDelibsMastersheet-InvitationalsEvals_Comments.csv`;
+	let finalInvScore = `https://relay-file-upload.s3-us-east-2.amazonaws.com/feb4bfdw93vUpsilonDelibsMastersheet-FinalTotalAverages(invitationalsprepandpres)(2).csv`;
+	let prepScoresCSV = `https://relay-file-upload.s3-us-east-2.amazonaws.com/iu7opcarxheUpsilonDelibsMastersheet-InvitationalsEvals_Comments(1).csv`;
 
-	let prepBreakdownCSV = `https://relay-file-upload.s3-us-east-2.amazonaws.com/6kyde4u8egqUpsilonDelibsMastersheet-Invitationalspreparationaverages.csv`;
+	let prepBreakdownCSV = `https://relay-file-upload.s3-us-east-2.amazonaws.com/ffl7yoqfhtUpsilonDelibsMastersheet-Invitationalspreparationaverages(3).csv`;
 	let presentaionBreakdownCSV = `https://relay-file-upload.s3-us-east-2.amazonaws.com/u5qex8m8wz9UpsilonDelibsMastersheet-Presentationsaverages(1).csv`;
 
-	let interviewUrl = `https://relay-file-upload.s3-us-east-2.amazonaws.com/16kivrxw9r2InterviewTemplate-Sheet1(1).csv`;
+	let interviewUrl = `https://relay-file-upload.s3-us-east-2.amazonaws.com/x1496ks4vbmInterviewTemplate-Sheet1(3).csv`;
 
 	let blacklist = ['', ' '];
+
+	const pastDelibs = [
+		{
+			name: 'Sean Davis',
+			notes: ``,
+		},
+		{
+			name: 'Jack Pereira',
+			notes: ``,
+		},
+		{
+			name: 'Aum Purohit',
+			notes: `Really like him! ***
+One of the most passionate rushees, willing to grow and learn
+Asked a lot of insightful questions about the different organizations the bros were in
+Connected himself very professionally every time – continued the same conversation at another event
+Would love to see how he works in a team
+Very engaged and passionate
+Very patient, asked good questions
+How would we test him at invitationals? - put him on a team with other leaders (passionate rushees) and see how he reacts, how much he’ll lead or step back 
+Resume is iffy (didn’t go to the event)
+Had a great answer for what he could bring to the chapter
+
+`,
+		},
+		{ name: 'Nichole Chen', notes: `` },
+		{
+			name: 'Megha Iyer',
+			notes: ``,
+		},
+		{ name: 'Brianna Giangrasso', notes: `` },
+		{ name: 'Rebekah Campus-Hartman', notes: `` },
+		{ name: 'Kayla Handley', notes: `` },
+		{ name: 'Patrick McMorrow', notes: `` },
+		{ name: 'Will Marris', notes: `` },
+		{ name: 'Galia Koonyevsky', notes: `` },
+		{ name: 'Isabella Lam', notes: `` },
+		{ name: 'Rishi Kamtam', notes: `` },
+		{ name: 'Pallavi Shankar', notes: `` },
+		{ name: 'Bonisha Maitra', notes: `` },
+		{ name: 'Ethan Kim', notes: `` },
+		{ name: 'Liam Clarke', notes: `` },
+		{ name: 'Tennyson Hopley-Romig', notes: `` },
+		{
+			name: 'Yuna Shin',
+			notes: `Good, quiet, very passionate about film and what she does
+Really inclusive in group conversations
+Talked a lot about her personal life
+Asked great questions
+Asked how she can stand out in the rush process, told her to connect with bros on LinkedIn and she did
+Good one on one, doesn’t talk at all in group settings
+Willingness to learn (3.2)
+Could she be a social fit? – overall very friendly
+What could we give her that she couldn’t get from Tamid? – very finance consulting driven. She’s not very involved with that
+Had a convo about My Little Pony … everyone else disagreed with her but she stood her ground !!!!
+Always a space for someone who’s quieter in a class
+`,
+		},
+		{ name: `Rhianna D'Silva`, notes: `` },
+		{
+			name: 'Amanda Gomes',
+			notes: `
+			
+			Presented herself well and asked great questions
+Want to see her in a professional setting even more
+Made no attempt to join in a conversation with three rushees – something we could test at invitationals
+Asked a lot of good professional questions, but kept interrupting a bro with other questions – would want to see her in a team setting
+Wants more professional help from AKPsi as a student that just switched into DMSB
+Seemed really genuine (even when she thought a brother was a rushee)
+Has a lot of IA and law experience – wants to focus on her IA major. A lot of experience that the brotherhood generally doesn’t have
+Took resume feedback well(ish)
+Has a clear goal for joining AKPsi that’s evident in her application
+
+`,
+		},
+		{ name: 'Tarun Singh', notes: `` },
+	];
 	let whitelist = [
 		`Sean Davis`,
 		`Jack Pereira`,
-		`Aum Pourohit`,
+		`Aum Purohit`,
 		`Nichole Chen`,
 		`Megha Iyer`,
 		`Brianna Giangrasso`,
